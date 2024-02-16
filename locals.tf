549a9f21b102372f18ebdff3bf137e31ca8e3d21
scotttyso <tyscott@cisco.com>
2024-02-16 13:43:55 -0500

    changing name spacer to /

--- locals.tf
+++ locals.tf
@@ -117,7 +117,7 @@ locals {
           pod_id        = v.pod_id
         }
       ]
-    ]) : "${i.node_id}:${i.mgmt_epg_type}:${i.management_epg}" => i
+    ]) : "${i.node_id}/${i.mgmt_epg_type}/${i.management_epg}" => i
   }
   ooband = {
     for i in flatten([
@@ -136,7 +136,7 @@ locals {
           pod_id        = v.pod_id
         }
       ]
-    ]) : "${i.node_id}:${i.mgmt_epg_type}:${i.management_epg}" => i
+    ]) : "${i.node_id}/${i.mgmt_epg_type}/${i.management_epg}" => i
   }
 
   static_node_mgmt_addresses = merge(local.inband, local.ooband)
